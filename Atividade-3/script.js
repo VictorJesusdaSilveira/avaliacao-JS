c43e0b64cabd40b499456a3f3b888ad844b6992e
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2025-10-10 10:00:54 -0300

    Create script.js

---
+++ Atividade-3/script.js
@@ -0,0 +1,3 @@
+let titulo = document.querySelector(".titulo").style.color = "darkgreen"
+document.querySelector(".titulo").style.fontSize = Math.floor(((Math.random() * (100 - 30) + 30))) + "px"
+document.querySelector(".titulo").innerText = "Olá Mundooooooo"
